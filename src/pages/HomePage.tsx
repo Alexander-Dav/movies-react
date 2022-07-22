4a0dccca89e09e6d8fb0580b60fcde08d17193db
Aleksandr Davidkin <cahek_441@mail.ru>
2022-07-22 17:12:15 +0600

    Full

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDebounce } from '../hooks/debounce';
 import { useSearchUsersQuery } from '../store/github/github.api';
 
 export default function HomePage() {
-  const { isLoading, isError, data } = useSearchUsersQuery('Alexander');
-  return <div>HomePage</div>;
+  const [search, setSearch] = useState('');
+  const debounce = useDebounce(search);
+  const { isLoading, isError, data } = useSearchUsersQuery(debounce, {
+    skip: debounce.length < 2,
+  });
+
+  useEffect(() => {
+    console.log(debounce);
+  }, [debounce]);
+
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      {isError && <p className="text-center text-red-600"> Something went wrong</p>}
+      <div className="relative w-[560px]">
+        <input
+          type="text"
+          className="border py-2 px-4 w-full h-[42px] mb-2"
+          placeholder="Search for Github username..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg"></div>
+      </div>
+    </div>
+  );
 }
